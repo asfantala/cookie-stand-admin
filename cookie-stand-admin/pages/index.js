9a39c12a362f7c27351b1d8ed650fb4b4734f34d
asfantala <22037049@student.ltuc.com>
2023-07-26 20:53:17 +0000

    version 3

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -1,43 +1,93 @@
-import Head from 'next/head';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuth } from "../contexts/auth";
+import LoginForm from '@/components/LoginForm';
 import Header from '@/components/Header';
 import Form from '@/components/Form';
 import Footer from '@/components/Footer';
 import ReportTable from '@/components/Table';
 import { hourlySalesData } from '@/data';
-function home() {
-  //  placeholder component showing JSON string of last created Cookie Stand.
-  // const [lastCookieStand, setLastCookieStand] = useState(null);
-  const [cookieStands, setCookieStands] = useState([]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const entry = {
-      id: cookieStands.length + 1,
-      location: form.get('location'),
-      minCustomers: form.get('minCustomers'),
-      maxCustomers: form.get('maxCustomers'),
-      avgCookies: form.get('avgCookies'),
-      hourly_sales: hourlySalesData,
-    };
-    setCookieStands([...cookieStands, entry]);
+function Home() {
+  const [cookieStands, setCookieStands] = useState([]);
+  const { tokens, login, user } = useAuth();
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  const handleSubmit = async (event) => {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const entry = {
+    id: cookieStands.length + 1,
+    location: form.get('location'),
+    minCustomers: form.get('minCustomers'),
+    maxCustomers: form.get('maxCustomers'),
+    avgCookies: form.get('avgCookies'),
+    hourly_sales: hourlySalesData,
+  };
+
+  try {
+    const response = await fetch(baseUrl + 'api/v1/cookiestands/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${tokens?.access}`,
+      },
+      body: JSON.stringify(entry),
+    });
+
+    // Check if the request was successful (status code 2xx)
+    if (response.ok) {
+      setCookieStands([...cookieStands, entry]);
+
+      // Fetch the updated data after creating a new entry
+      updateCookies();
+    } else {
+      console.error('Failed to create entry:', response);
+    }
+  } catch (error) {
+    console.error('Error creating entry:', error);
   }
+};
+
+
+ 
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(baseUrl + `api/v1/cookiestands/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${tokens?.access}`
+        },
+      });
+
+      if (response.ok) {
+        setCookieStands(cookieStands.filter((stand) => stand.id !== id));
+        console.log(`Deleted ${id}`);
+      } else {
+        console.error('Failed to delete entry:', response.status);
+      }
+    } catch (error) {
+      console.error('Error deleting entry:', error);
+    }
+  };
 
   const numLocations = cookieStands.length;
 
   return (
-    <div >
-      <Head >
-        <title>Cookie Stand Admin</title>
-      </Head>
-      <Header />
-      <main className="container mx-auto p-4">
-        <Form onSubmit={handleSubmit} />
-        {/* <Placeholder lastCookieStand={lastCookieStand} /> */}
-        <ReportTable reports={cookieStands} />
-      </main>
-      <Footer numLocations={numLocations} />
+    <div>
+      {user ? (
+        <>
+          <Header />
+          <main className="container mx-auto p-4">
+            <Form onSubmit={handleSubmit} />
+            <ReportTable reports={cookieStands} onDelete={handleDelete} />
+          </main>
+          <Footer numLocations={numLocations} />
+        </>
+      ) : (
+        <LoginForm onLogin={login} />
+      )}
     </div>
   );
 }
@@ -55,4 +105,4 @@ function Placeholder(props) {
     </div>
   );
 }
-export default home;
+export default Home;
